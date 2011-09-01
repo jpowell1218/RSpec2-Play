088cbac1d00778a4d2f91345010679afe22d0d3a
Jeff Powell <powellj@mac.com>
2011-09-01 06:25:41 -0600

    Basic app setup

---
+++ spec/book_spec.rb
@@ -0,0 +1,41 @@
+# run via rspec spec
+
+require 'spec_helper'
+
+describe Book do
+
+  before :each do
+    @book = Book.new "Title", "Author", :category
+  end
+
+  describe "#new" do
+    it "returns a new book object" do
+      @book.should(be_an_instance_of(Book))
+    end
+
+    it "throws an ArgumentError when given fewor than 3 parameters" do
+      lambda { Book.new "Title", "Author" }.should raise_exception ArgumentError
+    end
+  end
+
+  describe "#title" do
+    it "returns the correct title" do
+      @book.title.should eql "Title"
+    end
+  end
+
+  describe "#author" do
+    it "returns the correct author" do
+      @book.author.should eql "Author"
+    end
+  end
+
+  describe "#category" do
+    it "returns the correct category" do
+      @book.category.should eql :category
+    end
+  end
+
+
+
+end
