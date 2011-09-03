25e2f00f40e618e07e9281422f7b164fe174bc3a
Jeff Powell <powellj@mac.com>
2011-09-03 09:12:09 -0600

    Initial implementation of the library class & spec

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,4 +1,4 @@
-# stopped at 18:15
+# run just this spec via: rspec spec/library_spec.rb --format nested
 
 require 'spec_helper'
 
@@ -6,15 +6,34 @@ describe "Library Object" do
 
   before :all do
     lib_arr = [
-      Book.net("Javascript tho good parts", "Douglas Crawford", :development),
-      Book.net("Designing with web standards", "Jeffrey Zeldman", :design),
-      Book.net("Don't Make Me Think", "Steve Krug", :development),
-      Book.net("Javascript Patterns", "Stoya Stefanov", :development),
-      Book.net("Responsive Web Design", "Ethan Marcotte", :design)
+      Book.new("Javascript tho good parts", "Douglas Crawford", :development),
+      Book.new("Designing with web standards", "Jeffrey Zeldman", :design),
+      Book.new("Don't Make Me Think", "Steve Krug", :development),
+      Book.new("Javascript Patterns", "Stoya Stefanov", :development),
+      Book.new("Responsive Web Design", "Ethan Marcotte", :design)
     ]
+
+    File.open "books.yml", "w" do |f|
+      f.write YAML::dump lib_arr
+    end
   end
 
   before :each do
+    @lib = Library.new "books.yml"
+  end
 
+  describe "#new" do
+    context "with no parameters" do
+      it "has no books" do
+        lib = Library.new
+        lib.should have(0).books # alternatively: lib.books.length.should == 0
+      end
+    end
+
+    context "with a yaml file name parameter" do
+      it "has five books" do
+        @lib.should have(5).books
+      end
+    end
   end
 end
